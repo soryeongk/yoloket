87007894ec3e68e55cfbacf2330c8ab02a3d2460
soryeongk <soryeongk.kr@gmail.com>
2022-02-22 19:39:37 +0900

    :construction: init: add assets

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as comment } from './comment.svg';
+export { default as download } from './download.svg';
+export { default as logo } from './logo.svg';
+export { default as store } from './store.svg';
