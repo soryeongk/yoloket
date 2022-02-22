b6a4a84d018b38d67aa4301bfe18b68063296f3a
soryeongk <soryeongk.kr@gmail.com>
2022-02-22 22:13:39 +0900

    :tada: feat: 제품 상세 왼쪽영역 퍼블리싱 완료

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -1,20 +1,74 @@
+import styled from "styled-components";
+
+import { store } from "../../assets";
+
 export default function ProductInfo() {
   return (
-    <section>
-      <img src="imgUrl" alt="제품 이미지" />
-      <article>
-        <img src="imgUrl" alt="판매자 이미지" />
-        <div>
-          <div>
-            <img src="imgUrl" alt="판매자 아이콘" />
-            <h2>욜로마켓</h2>
-          </div>
-          <h3>
+    <StSection>
+      <StProductImage src="imgUrl" alt="제품 이미지" />
+      <StArticle>
+        <StStoreImage />
+        <StStoreWrapper>
+          <StStoreTitle>
+            <img src={store} alt="판매자 아이콘" />
+            <StStoreName>욜로마켓</StStoreName>
+          </StStoreTitle>
+          <StStoreDesc>
             욜로마켓에서 판매하는 모든 제품은 미국 매장에서 직배송해드리는 정품입니다. 언제든 편하게 문의 주시길
             바랍니다!
-          </h3>
-        </div>
-      </article>
-    </section>
+          </StStoreDesc>
+        </StStoreWrapper>
+      </StArticle>
+    </StSection>
   );
 }
+
+const StSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  width: 50rem;
+`;
+
+const StProductImage = styled.img`
+  width: 100%;
+  height: 50rem;
+`;
+
+const StArticle = styled.article`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+`;
+
+const StStoreImage = styled.div`
+  border-radius: 4rem;
+  background-color: gray;
+
+  width: 8rem;
+  height: 8rem;
+`;
+
+const StStoreWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+
+  margin-left: 3rem;
+`;
+
+const StStoreTitle = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StStoreName = styled.h1`
+  ${({ theme }) => theme.fonts.body400}
+`;
+
+const StStoreDesc = styled.h2`
+  ${({ theme }) => theme.fonts.caption200}
+`;
