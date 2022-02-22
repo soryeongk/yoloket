97c1f532109e13b1cfa90c20dd97dc0ae91db258
soryeongk <soryeongk.kr@gmail.com>
2022-02-23 00:11:36 +0900

    :tada: feat: 메인 페이지 html 구조

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,3 +1,41 @@
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <main>
+      <h1>욜로켓 인기 상품</h1>
+      <section>
+        <article>
+          <img src="image" alt="제품 썸네일" />
+          <div>
+            <h2>[인기 상품 1위] 욜로켓 시그니처 디자인 제품</h2>
+            <h2>18,900원</h2>
+            <h3>미국 | 해외 무료 배송</h3>
+          </div>
+        </article>
+        <article>
+          <img src="image" alt="제품 썸네일" />
+          <div>
+            <h2>[인기 상품 1위] 욜로켓 시그니처 디자인 제품</h2>
+            <h2>18,900원</h2>
+            <h3>미국 | 해외 무료 배송</h3>
+          </div>
+        </article>
+        <article>
+          <img src="image" alt="제품 썸네일" />
+          <div>
+            <h2>[인기 상품 1위] 욜로켓 시그니처 디자인 제품</h2>
+            <h2>18,900원</h2>
+            <h3>미국 | 해외 무료 배송</h3>
+          </div>
+        </article>
+        <article>
+          <img src="image" alt="제품 썸네일" />
+          <div>
+            <h2>[인기 상품 1위] 욜로켓 시그니처 디자인 제품</h2>
+            <h2>18,900원</h2>
+            <h3>미국 | 해외 무료 배송</h3>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
